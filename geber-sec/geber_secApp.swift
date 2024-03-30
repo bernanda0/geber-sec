830b87193333cf79df374235af78d18ad16b81c3
bernanda-rai <inotbnri@gmail.com>
2024-03-31 02:21:27 +0700

    Initial Commit

---
+++ geber-sec/geber_secApp.swift
@@ -0,0 +1,17 @@
+//
+//  geber_secApp.swift
+//  geber-sec
+//
+//  Created by bernanda on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct geber_secApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ geber-secUITests/geber_secUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  geber_secUITestsLaunchTests.swift
+//  geber-secUITests
+//
+//  Created by bernanda on 31/03/24.
+//
+
+import XCTest
+
+final class geber_secUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
